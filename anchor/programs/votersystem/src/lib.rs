108cfc7b8d4792408c8e53715dccf87e0cb952c9
Emmanuel Pius <emmycode4495@gmail.com>
2024-10-25 05:24:58 +0100

    Creating the basic structure for the candidate and polls funtionalty

--- anchor/programs/votersystem/src/lib.rs
+++ anchor/programs/votersystem/src/lib.rs
@@ -8,63 +8,89 @@ declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 pub mod votersystem {
     use super::*;
 
-  pub fn close(_ctx: Context<CloseVotersystem>) -> Result<()> {
-    Ok(())
-  }
+    // Initialize the poll
+    pub fn initialize_poll(ctx: Context<InitializePoll>, 
+                             poll_id: u64,
+                             description:String,
+                            poll_start: u64,
+                            poll_end: u64,
+                         )
+    -> Result<()>{
+      let poll = &mut ctx.accounts.poll;
+      poll.poll_id = poll_id;
+      poll.description = description;
+      poll.poll_start = poll_start;
+      poll.poll_end = poll_end;
+      poll.candidate_amount = 0;
+      Ok(())
+    }
 
-  pub fn decrement(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.votersystem.count = ctx.accounts.votersystem.count.checked_sub(1).unwrap();
-    Ok(())
-  }
+    pub fn initialize_candidate(ctx: Context<InitializeCandidate>,
+                                candidate_name: String,
+                                poll_id: u64) -> Result<()>{
+        
+         Ok(())                         
 
-  pub fn increment(ctx: Context<Update>) -> Result<()> {
-    ctx.accounts.votersystem.count = ctx.accounts.votersystem.count.checked_add(1).unwrap();
-    Ok(())
-  }
+    }
 
-  pub fn initialize(_ctx: Context<InitializeVotersystem>) -> Result<()> {
-    Ok(())
-  }
-
-  pub fn set(ctx: Context<Update>, value: u8) -> Result<()> {
-    ctx.accounts.votersystem.count = value.clone();
-    Ok(())
-  }
 }
 
+// ACCOUNTS STRUCT FOR THE CANDIDATE
 #[derive(Accounts)]
-pub struct InitializeVotersystem<'info> {
+#[instruction(candidate_name: String, poll_id: u64)]
+pub struct InitializeCandidate<'info>{
   #[account(mut)]
-  pub payer: Signer<'info>,
+  pub signer: Signer<'info>,
 
   #[account(
-  init,
-  space = 8 + Votersystem::INIT_SPACE,
-  payer = payer
+    seeds = [poll_id.to_le_bytes().as_ref()],
+    bump
   )]
-  pub votersystem: Account<'info, Votersystem>,
+  pub poll: Account<'info, Poll>,
+
+  #[account(
+    init,
+    payer = signer,
+    space = 8 + Candidate::INIT_SPACE,
+    seeds = [poll_id.to_le_bytes().as_ref(), candidate_name.as_bytes()],
+    bump
+  )]
+  pub candidate: Account<'info, Candidate>,
   pub system_program: Program<'info, System>,
 }
-#[derive(Accounts)]
-pub struct CloseVotersystem<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
+#[account]
+#[derive(InitSpace)]
+pub struct Candidate{
+  pub candidate_name: String,
+  pub candidate_votes: u64
+}
 
+// ACCOUNTS STRUCT FOR THE POLL
+#[derive(Accounts)]
+#[instruction(poll_id:u64)]
+pub struct InitializePoll<'info>{
+  #[account(mut)]
+  pub signer: Signer<'info>,
   #[account(
-  mut,
-  close = payer, // close account and return lamports to payer
+    init,
+    payer = signer,
+    space = 8 + Poll::INIT_SPACE,
+    seeds = [poll_id.to_le_bytes().as_ref()],
+    bump,
   )]
-  pub votersystem: Account<'info, Votersystem>,
+  pub poll: Account<'info, Poll>,
+
+  pub system_program: Program<'info, System>
 }
 
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub votersystem: Account<'info, Votersystem>,
-}
 
 #[account]
 #[derive(InitSpace)]
-pub struct Votersystem {
-  count: u8,
+pub struct Poll{
+  pub poll_id: u64,
+  #[max_len(280)]
+  pub description: String,
+  pub poll_start: u64,
+  pub poll_end: u64,
+  pub candidate_amount: u64
 }
--- anchor/tests/votersystem.spec.ts
+++ anchor/tests/votersystem.spec.ts
@@ -1,77 +1,46 @@
 import * as anchor from '@coral-xyz/anchor'
 import {Program} from '@coral-xyz/anchor'
-import {Keypair} from '@solana/web3.js'
+import {Keypair, PublicKey} from '@solana/web3.js'
 import {Votersystem} from '../target/types/votersystem'
-import '@types/jest';
+import { startAnchor } from "solana-bankrun";
+import { BankrunProvider } from "anchor-bankrun";
+import { publicKey } from '@coral-xyz/anchor/dist/cjs/utils';
+
+
+
+const IDL = require('../target/idl/votersystem.json');
+const votingAddress = new PublicKey("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
 
 describe('votersystem', () => {
-  // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env()
-  anchor.setProvider(provider)
-  const payer = provider.wallet as anchor.Wallet
-
-  const program = anchor.workspace.Votersystem as Program<Votersystem>
-
-  const votersystemKeypair = Keypair.generate()
-
+  
   it('Initialize Votersystem', async () => {
-    await program.methods
-      .initialize()
-      .accounts({
-        votersystem: votersystemKeypair.publicKey,
-        payer: payer.publicKey,
-      })
-      .signers([votersystemKeypair])
-      .rpc()
+    const context = await startAnchor("", [{name:"voting", programId:votingAddress }], []);
+    const provider = new BankrunProvider(context);
 
-    const currentCount = await program.account.votersystem.fetch(votersystemKeypair.publicKey)
+    const votingProgram = new Program<Votersystem>(
+      IDL,
+      provider,
+    );
 
-    expect(currentCount.count).toEqual(0)
-  })
+    await votingProgram.methods.initializePoll(
+      new anchor.BN(1),
+      "Who is your favourite candidate",
+      new anchor.BN(0),
+      new anchor.BN(1728836508),
+    ).rpc();
 
-  it('Increment Votersystem', async () => {
-    await program.methods.increment().accounts({ votersystem: votersystemKeypair.publicKey }).rpc()
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8)],
+      votingAddress,
+    )
 
-    const currentCount = await program.account.votersystem.fetch(votersystemKeypair.publicKey)
+    const poll = await votingProgram.account.poll.fetch(pollAddress);
+    console.log(poll);
 
-    expect(currentCount.count).toEqual(1)
-  })
-
-  it('Increment Votersystem Again', async () => {
-    await program.methods.increment().accounts({ votersystem: votersystemKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.votersystem.fetch(votersystemKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(2)
-  })
-
-  it('Decrement Votersystem', async () => {
-    await program.methods.decrement().accounts({ votersystem: votersystemKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.votersystem.fetch(votersystemKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(1)
-  })
-
-  it('Set votersystem value', async () => {
-    await program.methods.set(42).accounts({ votersystem: votersystemKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.votersystem.fetch(votersystemKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(42)
-  })
-
-  it('Set close the votersystem account', async () => {
-    await program.methods
-      .close()
-      .accounts({
-        payer: payer.publicKey,
-        votersystem: votersystemKeypair.publicKey,
-      })
-      .rpc()
-
-    // The account should no longer exist, returning null.
-    const userAccount = await program.account.votersystem.fetchNullable(votersystemKeypair.publicKey)
-    expect(userAccount).toBeNull()
-  })
-})
+    expect(poll.pollId.toNumber()).toEqual(1);
+    expect(poll.description).toEqual("Answer Anything");
+    expect(poll.pollStart).toBeLessThan(poll.pollEnd.toNumber());
+    
+    
+  });
+});
